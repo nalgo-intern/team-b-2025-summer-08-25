ff27b145fb21ca26879c4eca4e4d1c8806eb163b
sakurai-shunsuke-nalgo-intern <sakurai.shunsuke.nalgo.intern@gmail.com>
2025-08-27 17:02:24 +0900

    button_repair

--- appdev/StartScreen.py
+++ appdev/StartScreen.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 
 # --- switch_frame : 画面を切り替えるための関数
+# 選択した難易度、アシストの有無を保存
 
 class AppState:
     mode = False
@@ -10,39 +12,55 @@ class StartScreen(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        tk.Label(self, text="これはスタート画面(仮)です", font=("Arial", 16)).pack(pady=20)
+        master.geometry("1200x600")
+        tk.Label(self.master, text="これはスタート画面(仮)です", font=("Arial", 16)).pack(pady=20)
+        
 
         self.mode_var = tk.BooleanVar(value=False)  # 初期値: False
         self.mode_button = tk.Checkbutton(
             self,
-            text="Mode Select (ON/OFF)",
+            text="アシストモード",
             variable=self.mode_var,
             onvalue=True,
             offvalue=False,
             indicatoron=False,  # ← トグルボタン風
             width=20,
-            height=2
+            height=2,
+            command=self.check_assist
         )
-        self.mode_button.pack(pady=10)
+        self.mode_button.place(relx=0.5, rely=0.8, x=-10)
 
         # スタートボタン
-        self.start_button = tk.Button(self, text="スタート画面", command=self.show_choices)
-        self.start_button.pack(pady=20)
+        self.start_button = tk.Button(
+            self.master,
+            text="スタート画面",
+            width=40,
+            height=5,
+            command=self.show_choices
+            )
+        self.start_button.place(relx=0.5, rely=0.2, x=-140)
 
         # 選択肢を置くための空間
         self.choice_frame = tk.Frame(self)
         self.choice_frame.pack(pady=10)
 
-        self.options = ["かんたん", "ふつう", "むずかしい"]
+        self.options = ["Easy", "Medium", "Hard"]
         self.selected_var = tk.IntVar(value=-1)
 
-        tk.Button(root, text="状態を確認(デバッグ用)", command=self.check_mode).pack(pady=10)
-        tk.Button(root, text="遊び方説明", command=self.make_info).pack(pady=10)
+        self.debug_button = tk.Button(root, text="状態を確認(デバッグ用)", command=self.check_mode)
+        self.introduction = tk.Button(root, text="遊び方説明", command=self.make_info)
+        self.debug_button.pack(pady=20, side = "left")
+        self.introduction.place(relx=0.5, rely=0.8, x=-20)
 
         # 決定ボタン
         self.confirm_button = tk.Button(self, text="決定", command=self.save_selection)
         # 最初は非表示
         self.confirm_button.pack_forget()
+
+        # アプリ終了ボタン
+        master.update_idletasks()
+        self.quit_button = tk.Button(self.master, text="アプリを閉じる", command=self.master.destroy)
+        self.quit_button.place(relx=1.0, rely=0.0, anchor="ne", x=-5, y=5)
     
     def get_mode(self):
         return self.mode_var.get()
@@ -56,12 +74,19 @@ class StartScreen(tk.Frame):
         tk.Button(info_win, text="閉じる", command=info_win.destroy).pack(pady=10)
 
     # メインから状態を確認するためのボタン
-    def check_mode():
+    def check_mode(self):
         print("ModeSelect の状態:", app.get_mode())
 
     # 説明用のウィンドウを作成
-    def make_info():
-        app.show_info()
+    def make_info(self):
+        self.show_info()
+
+    # アシストの有無の状態をスタート画面に表示
+    def check_assist(self):
+        if self.mode_var.get():
+            self.mode_button.config(text="アシスト：あり")
+        else:
+            self.mode_button.config(text="アシスト：なし")
 
     def show_choices(self):
         # スタートボタンを無効化
@@ -70,18 +95,20 @@ class StartScreen(tk.Frame):
         # 選択肢のボタンを動的生成
         for i, option in enumerate(self.options):
             btn = tk.Button(
-                self.choice_frame,
+                self.master,
                 text=option,
                 width=20,
                 height=2,
                 command=lambda index=i: self.save_selection(index)
             )
-            btn.pack(pady=5)
+            btn.place(relx=0.5, rely=0.4+0.08*i, x=-50)
     
     # 選択した難易度を保存
     def save_selection(self, index):
         AppState.selected_index = index
+        AppState.mode = self.get_mode()
         print("DifficultIndex: ", AppState.selected_index)
+        print("Assist: ", AppState.mode)
 
 
 
